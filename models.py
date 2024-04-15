d82f659291826419d3e2c28a739295fa9b1ec7cb
Spencer Whitehead <53270623+spwhitehead@users.noreply.github.com>
2024-04-15 16:57:57 -0600

    Initial migration

--- models.py
+++ models.py
@@ -4,5 +4,5 @@ from sqlmodel import Field, SQLModel, create_engine
 class Task(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     name: str
-    # task_description: str
-    # urgency: int
+    task_description: str
+    urgency: int
